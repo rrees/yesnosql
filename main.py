46035febbff18453e3609f63e1bb8d231ec1333f
Robert Rees <shudderfix@gmail.com>
2017-04-14 00:14:24 +0100

    Converts to using webapp2 in the main application

--- main.py
+++ main.py
@@ -1,33 +1,12 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
-from handlers import Questionnaire, Examples, FlowchartStart, Flowchart, Home
+import webapp2
 
-def main():
-	handlers = [('/', Home),
-	('/questionnaire', Questionnaire),
-	('/examples', Examples),
-	('/flowchart', FlowchartStart),
-	(r'/flowchart/(.+)', Flowchart),
+import handlers
+
+routes = [('/', handlers.Home),
+	('/questionnaire', handlers.Questionnaire),
+	('/examples', handlers.Examples),
+	('/flowchart', handlers.FlowchartStart),
+	(r'/flowchart/(.+)', handlers.Flowchart),
 	]
-	application = webapp.WSGIApplication(handlers, debug=True)
-	util.run_wsgi_app(application)
 
-
-if __name__ == '__main__':
-    main()
+app = webapp2.WSGIApplication(routes, debug=True)
